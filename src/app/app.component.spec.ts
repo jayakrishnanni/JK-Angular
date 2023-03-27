7cbbab9bf502d5399d9a194c8c1e5d914d5b603d
JAYAKRISHNAN NI <jayakrishnanni@gmail.com>
2023-03-27 21:11:14 +0530

    resolved the conflict

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,27 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
   it(`should have as title 'Chipmunk'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Chipmunk');
-=======
-  it(`should have as title 'Chipmunk-Assesment-Test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Chipmunk-Assesment-Test');
->>>>>>> origin
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<< HEAD
     expect(compiled.querySelector('.content span')?.textContent).toContain('Chipmunk app is running!');
-=======
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Chipmunk-Assesment-Test app is running!');
->>>>>>> origin
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'Chipmunk';
-=======
-  title = 'Chipmunk-Assesment-Test';
->>>>>>> origin
 
 
 }
